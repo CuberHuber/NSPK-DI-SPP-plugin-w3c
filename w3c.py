39ab91d7f877ec60763bb928760b3aaabf9e1c03
CuberHuber <lupashcko.rom@yandex.ru>
2023-10-26 22:40:01 +0500

    init commit. перенес парсер, отредактировал код

--- w3c.py
+++ w3c.py
@@ -6,11 +6,14 @@
 import logging
 import os
 import time
+from datetime import datetime
+
+from selenium.webdriver.common.by import By
 
 from src.spp.types import SPP_document
 
 
-class SOURCE_PARSER_CLASS:
+class W3C:
     """
     Класс парсера плагина SPP
 
@@ -22,10 +25,11 @@ class SOURCE_PARSER_CLASS:
 
     """
 
-    SOURCE_NAME = '<unique source name>'
+    SOURCE_NAME = 'w3c'
     _content_document: list[SPP_document]
+    HOST = 'https://www.w3.org/TR/'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, driver, *args, **kwargs):
         """
         Конструктор класса парсера
 
@@ -34,6 +38,9 @@ class SOURCE_PARSER_CLASS:
         """
         # Обнуление списка
         self._content_document = []
+
+        # Установка selenium driver
+        self.driver = driver
 
         # Логер должен подключаться так. Вся настройка лежит на платформе
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -59,14 +66,125 @@ class SOURCE_PARSER_CLASS:
         :rtype:
         """
         # HOST - это главная ссылка на источник, по которому будет "бегать" парсер
-        self.logger.debug(F"Parser enter to {HOST}")
+        self.logger.debug(F"Parser enter to {self.HOST}")
 
         # ========================================
         # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
         # -
+        self.driver.get('https://www.w3.org/TR/')
+        doc_list = self.driver.find_elements(By.CLASS_NAME, 'tr-list__item__header')
+        for doc in doc_list:
+
+            # Ссылка на документ
+            __doc_link = doc.find_element(By.TAG_NAME, 'a').get_attribute('href')
+
+            try:
+                __title = doc.find_element(By.TAG_NAME, 'a').text
+            except:
+                # завершение обработки документа, переход к следующему
+                self.logger.exception(f'Ошибка при обработке документа {__doc_link}')
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
+                continue
+
+
+            # pub_date
+            __pub_date = datetime.strptime(doc.find_element(By.XPATH, '..//time').get_attribute('datetime'), '%Y-%m-%d')
+
+            # tags
+            tags_el = doc.find_elements(By.XPATH, '..//*[contains(text(), \'Tags\')]/../dd')
+            __tags = [x.text for x in tags_el]
+
+            # deliverers (workgroup)
+            deliverers_el = doc.find_elements(By.XPATH, '..//*[contains(text(), \'Deliverers\')]/../dd')
+            __devilverers = [x.text for x in deliverers_el]
+
+            # family
+            __family = doc.find_element(By.XPATH, '../../h2').text
+
+            self.driver.execute_script("window.open('');")
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.driver.get(__doc_link)
+            time.sleep(0.5)
+
+            # abstract
+            try:
+                __abstract = self.driver.find_element(By.ID, 'abstract').text
+            except:
+                # Удален np.nan
+                __abstract = None
+
+            # text_content
+            __text_content = self.driver.find_element(By.TAG_NAME, 'body').text
+
+            # web_link
+            try:
+                __web_link = self.driver.find_element(By.XPATH,
+                                                    '//dt[contains(text(), \'This version\')]/following-sibling::dd[1]//a').get_attribute(
+                    'href')
+            except:
+                self.logger.exception(
+                    f'Не удалось получить веб-ссылку на версию документа за определенную дату. В web_link вносится общая ссылка {__doc_link}')
+                __web_link = __doc_link
+
+            # doc_type
+            try:
+                __doc_type = self.driver.find_element(By.XPATH, '//p[@id = \'w3c-state\']/a').text
+            except:
+                # удален np.nan
+                __doc_type = None
+
+            # authors
+            authors_el = self.driver.find_elements(By.XPATH,
+                                                   '//dt[contains(text(), \'Authors\')]/following-sibling::dd[@class=\'editor p-author h-card vcard\']')
+            __authors = [x.text for x in authors_el]
+
+            # authors
+            editors_el = self.driver.find_elements(By.XPATH,
+                                                   '//dt[contains(text(), \'Editors\')]/following-sibling::dd[@class=\'editor p-author h-card vcard\']')
+            __editors = [x.text for x in editors_el]
+
+            # commit
+            commit_links = self.driver.find_elements(By.XPATH, '//a[contains(text(), \'Commit history\')]')
+            if len(commit_links) > 0:
+                self.driver.get(commit_links[0].get_attribute('href'))
+                time.sleep(1)
+                commit_el = self.driver.find_elements(By.XPATH,
+                                                      '//div[@class=\'TimelineItem TimelineItem--condensed pt-0 pb-2\']//p[contains(@class,\'mb-1\')]')
+                __commits = [x.text for x in commit_el]
+            else:
+                # удален np.nan
+                __commits = None
+
+            __spp_doc = SPP_document(
+                doc_id=None,
+                title=__title,
+                abstract=__abstract,
+                text=__text_content,
+                web_link=__web_link,
+                local_link=None,
+                other_data={
+                    'doc_type': __doc_type,
+                    'devilverers': __devilverers,
+                    'authors': __authors,
+                    'tags': __tags,
+                    'commits': __commits,
+                    'family': __family,
+                    'editors': __editors,
+                },
+                pub_date=__pub_date,
+                load_date=None,
+            )
+
+            self._content_document.append(__spp_doc)
+
+            self.logger.info(self._find_document_text_for_logger(__spp_doc))
+
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
 
         # Логирование найденного документа
-        self.logger.info(self._find_document_text_for_logger(document))
+        # self.logger.info(self._find_document_text_for_logger(document))
 
         # ---
         # ========================================
